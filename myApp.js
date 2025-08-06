e46c9ff2b878b84913c2f0f709450fe4cd01f3e2
orange <orangemyeni@gmail.com>
2025-08-06 23:01:44 +0200

    push

--- myApp.js
+++ myApp.js
@@ -2,10 +2,15 @@ let express = require('express');
 let app = express();
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/style.css");
 });
 
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
