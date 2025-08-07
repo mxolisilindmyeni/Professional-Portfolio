30903f02ec47999613b45729f001179dd380eef8
Mxolisi <94915680+mxolisilindmyeni@users.noreply.github.com>
2025-08-07 20:48:09 +0000

    add dockerfile

--- myApp.js
+++ myApp.js
@@ -10,12 +10,26 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get('/healthcheck', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // If you want a /public route for assets
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM, shutting down gracefully');
+  process.exit(0);
+});
 
-
+//this peace os code 
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Simple server listening on ${port}`);
+});
+// Keep process alive
+setInterval(() => {}, 1000)
 
 
 
